73f88dd1ed2aa2d224feb67cb2c39e3ed5538b1d
zelkova <cohiba666@gmail.com>
2025-09-23 00:38:38 +0300

    common pipeline

--- packages/notifier/bot.py
+++ packages/notifier/bot.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from telebot import TeleBot
 from vyper import v
@@ -8,11 +9,23 @@ v.add_config_path(config)
 v.read_in_config()
 
 
-def send_file() -> None:
+# def send_file() -> None:
+#     telegram_bot = TeleBot(v.get("telegram.token"))
+#     file_path = Path(__file__).parent.joinpath("../..").joinpath("swagger-coverage-report.html")
+#     with open(file_path, "rb") as document:
+#         telegram_bot.send_document(v.get("telegram.chat_id"), document=document, caption="coverage")
+
+def send_file():
     telegram_bot = TeleBot(v.get("telegram.token"))
-    file_path = Path(__file__).parent.joinpath("../..").joinpath("swagger-coverage-report.html")
-    with open(file_path, "rb") as document:
-        telegram_bot.send_document(v.get("telegram.chat_id"), document=document, caption="coverage")
+    print("Current working directory:", os.getcwd())
+    file_path = "../../swagger-coverage-report.html"
+    absolute_path = os.path.abspath(file_path)
+    print(f"Attempting to open file at: {absolute_path}")
+    try:
+        with open(absolute_path, "rb") as document:
+            telegram_bot.send_document(v.get("telegram.chat_id"), document=document, caption="coverage")
+    except Exception as e:
+        print(f"An error occurred while sending the file: {e}")
 
 
 if __name__ == "__main__":
