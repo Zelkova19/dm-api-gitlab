2096b361884b2e9aea026b918a95277ce420c9c4
zelkova <cohiba666@gmail.com>
2025-09-23 08:22:11 +0300

    lint

--- packages/notifier/bot.py
+++ packages/notifier/bot.py
@@ -12,25 +12,13 @@ v.read_in_config()
 def send_file() -> None:
     telegram_bot = TeleBot(v.get("telegram.token"))
     print("Current working directory:", os.getcwd())
-    # file_path = Path(__file__).parent.joinpath("../..").joinpath("swagger-coverage-report.html")
-    project_root = Path(__file__).parent.parent.parent
-    file_path = project_root.parent.parent / "swagger-coverage-report.html"
+    file_path = Path(__file__).parent.joinpath("../..").joinpath("swagger-coverage-report.html")
     print(f"Attempting to open file at: {file_path}")
-
-    with open(file_path, "rb") as document:
-        telegram_bot.send_document(v.get("telegram.chat_id"), document=document, caption="coverage")
-
-# def send_file():
-#     telegram_bot = TeleBot(v.get("telegram.token"))
-#     print("Current working directory:", os.getcwd())
-#     file_path = "../../swagger-coverage-report.html"
-#     absolute_path = os.path.abspath(file_path)
-#     print(f"Attempting to open file at: {absolute_path}")
-#     try:
-#         with open(absolute_path, "rb") as document:
-#             telegram_bot.send_document(v.get("telegram.chat_id"), document=document, caption="coverage")
-#     except Exception as e:
-#         print(f"An error occurred while sending the file: {e}")
+    try:
+        with open(file_path, "rb") as document:
+            telegram_bot.send_document(v.get("telegram.chat_id"), document=document, caption="coverage")
+    except FileNotFoundError:
+        print(f"File '{file_path}' does not exist.")
 
 
 if __name__ == "__main__":
