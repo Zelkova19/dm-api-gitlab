9ac17f437c5e6e6c71a994a00eb3adf5d6783c75
zelkova <cohiba666@gmail.com>
2025-09-23 08:08:09 +0300

    common pipeline

--- packages/notifier/bot.py
+++ packages/notifier/bot.py
@@ -9,23 +9,26 @@ v.add_config_path(config)
 v.read_in_config()
 
 
-# def send_file() -> None:
-#     telegram_bot = TeleBot(v.get("telegram.token"))
-#     file_path = Path(__file__).parent.joinpath("../..").joinpath("swagger-coverage-report.html")
-#     with open(file_path, "rb") as document:
-#         telegram_bot.send_document(v.get("telegram.chat_id"), document=document, caption="coverage")
-
-def send_file():
+def send_file() -> None:
     telegram_bot = TeleBot(v.get("telegram.token"))
     print("Current working directory:", os.getcwd())
-    file_path = "../../swagger-coverage-report.html"
-    absolute_path = os.path.abspath(file_path)
-    print(f"Attempting to open file at: {absolute_path}")
-    try:
-        with open(absolute_path, "rb") as document:
-            telegram_bot.send_document(v.get("telegram.chat_id"), document=document, caption="coverage")
-    except Exception as e:
-        print(f"An error occurred while sending the file: {e}")
+    file_path = Path(__file__).parent.joinpath("../..").joinpath("swagger-coverage-report.html")
+    print(f"Attempting to open file at: {file_path}")
+
+    with open(file_path, "rb") as document:
+        telegram_bot.send_document(v.get("telegram.chat_id"), document=document, caption="coverage")
+
+# def send_file():
+#     telegram_bot = TeleBot(v.get("telegram.token"))
+#     print("Current working directory:", os.getcwd())
+#     file_path = "../../swagger-coverage-report.html"
+#     absolute_path = os.path.abspath(file_path)
+#     print(f"Attempting to open file at: {absolute_path}")
+#     try:
+#         with open(absolute_path, "rb") as document:
+#             telegram_bot.send_document(v.get("telegram.chat_id"), document=document, caption="coverage")
+#     except Exception as e:
+#         print(f"An error occurred while sending the file: {e}")
 
 
 if __name__ == "__main__":
